c8427c5cade900fbd4bb4347614eeeeffe8f7a52
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2023-12-12 13:18:32 -0300

    Update validacao.js

--- app/validacao.js
+++ app/validacao.js
@@ -5,6 +5,14 @@ function verificarSeOChutePossuiUmValorValido(chute) {
     elementoChute.innerHTML += "<div>Valor inválido </div>";
     return;
   }
+  if (chute.toUpperCase() === "GAME OVER") {
+    document.body.innerHTML = `
+        <h2>Game Over!!!</h2>
+        <h3>Pressione o botão para jogar novamente</h3>
+        <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+        `;
+    document.body.style.backgroundColor = "black";
+  }
   if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
     elementoChute.innerHTML += `<div> Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`;
     return;
