3150098fbd181599de7dc35402cc1c1be060834c
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2023-12-13 11:11:17 -0300

    Projeto finalizado

--- app/validacao.js
+++ app/validacao.js
@@ -1,17 +1,16 @@
-// const btnReiniciar = document.getElementById("jogar-novamente");
 function verificarSeOChutePossuiUmValorValido(chute) {
   const numero = +chute;
-  if (chuteInvalido(numero)) {
-    elementoChute.innerHTML += "<div>Valor inválido </div>";
-    return;
-  }
   if (chute.toUpperCase() === "GAME OVER") {
     document.body.innerHTML = `
-        <h2>Game Over!!!</h2>
+        <h2>GAME OVER!!!</h2>
         <h3>Pressione o botão para jogar novamente</h3>
         <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
         `;
     document.body.style.backgroundColor = "black";
+  }
+  if (chuteInvalido(numero)) {
+    elementoChute.innerHTML += "<div>Valor inválido </div>";
+    return;
   }
   if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
     elementoChute.innerHTML += `<div> Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`;
